c6f8a362be8d9642c523bda5c23003f10f88c7d3
Minh Nguyen <thanhnhatminhnguyen@gmail.com>
2019-03-01 21:54:38 -0800

    Adding Item, ItemPage components, add react-bootstrap and react-icons
    Adding Navbar and Footer to ItemPage
    Adding Layout, modal pop up that includes image carousel to item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,70 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import { Modal, Card } from 'react-bootstrap' 
 
 class Item extends Component {
+  state = {
+    showModal: false,
+  }
+
+  openCarousel = (e) => {
+    this.setState(() => ({
+      showModal: true
+    }))
+  }
+
+  closeCarousel = (e) => {
+    this.setState( () => ({
+      showModal: false
+    }))
+  }
+
   render() {
+    const { id, name, hero, images, priceRange, thumbnail } = this.props.item 
+    const fixedName = name.replace("Fibrosoft&#8482; ","")
+
+    const price = Math.round(Math.random() * (priceRange.selling.high - priceRange.selling.low))
+                  + priceRange.selling.low 
+    console.log(this.props.item)
     return (
-      <div>
-      </div>
+        <Card className="item-card">
+          <Modal 
+            show={this.state.showModal}
+            onHide={this.closeCarousel}>
+            <div className="carousel-container">
+              <Carousel>
+                {
+                  images.map((image) => (
+                    <Carousel.Item key={image.href}>
+                      <img
+                        src={image.href}
+                        alt={`Image`}
+                        className="d-block w-100"
+                        onClick={this.openCarousel} />
+                    </Carousel.Item>
+                  ))
+                }
+              </Carousel>
+            </div>
+          </Modal>
+          <Card.Header>
+          <div className="hero-image">
+              <img src={hero.href}
+                alt={`Picture of ${fixedName}`}
+                className=""
+                onClick={this.openCarousel} />
+                <h2><span>$ {price}</span></h2>
+            </div>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>
+              {fixedName}
+            </Card.Title>
+            <Card.Text>
+              Description for item {id}
+            </Card.Text>
+          </Card.Body>
+        </Card>
     )
   }
 }
--- src/components/ItemPage.js
+++ src/components/ItemPage.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import itemData from '../product-input.json'
 import Item from './Item'
+import { Navbar, Nav } from 'react-bootstrap'
 
 class ItemPage extends Component {
   state = {
     items: itemData
   }
 
+  componentDidMount() {
+    // Set the title for the page
+    document.title = "Listing Page"
+  }
+
   render() {
     const { items } = this.state
-    console.log(items)
 
     return (
-      <ul>
-        {
-          items.groups.map( (item) => (
-            <Item key={item.id} />
-          ))
-        }
-      </ul>
+      <div id="container">
+        <Navbar 
+          bg="dark" 
+          variant="dark">
+          <Navbar.Brand>Product Listing</Navbar.Brand>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link>Categories</Nav.Link>
+              <Nav.Link>Items</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link>Profile</Nav.Link>
+              <Nav.Link>Sign Out</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <ul className="item-page">
+          {
+            items.groups.map( (item) => (
+              <Item key={item.id} item={item} />
+            ))
+          }
+        </ul>
+        <Navbar 
+          bg="dark" 
+          variant="dark" 
+          fixed="bottom"
+          className="footer">
+          <Navbar.Brand>
+            Produced By Robert Nguyen@2019
+          </Navbar.Brand>
+        </Navbar>
+      </div>
     )
   }
 }
