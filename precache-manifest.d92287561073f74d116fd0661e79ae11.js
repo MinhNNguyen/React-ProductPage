437c47a43dd619a40bb3aea4ecbdc62a30998449
Minh Nguyen <thanhnhatminhnguyen@gmail.com>
2019-03-04 22:42:19 -0800

    Updates

---
+++ precache-manifest.d92287561073f74d116fd0661e79ae11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "463239bf48052567a155",
+    "url": "/React-ProductPage/static/js/runtime~main.463239bf.js"
+  },
+  {
+    "revision": "8318d2e515c09524edc9",
+    "url": "/React-ProductPage/static/js/main.8318d2e5.chunk.js"
+  },
+  {
+    "revision": "6a5e69c49c5d937da7ab",
+    "url": "/React-ProductPage/static/js/2.6a5e69c4.chunk.js"
+  },
+  {
+    "revision": "8318d2e515c09524edc9",
+    "url": "/React-ProductPage/static/css/main.b87d01bd.chunk.css"
+  },
+  {
+    "revision": "97d69f3b20c49983bd84b6f1fd487638",
+    "url": "/React-ProductPage/index.html"
+  }
+];
